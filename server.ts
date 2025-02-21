cd97adbc7db22a80b2de3838b6f9970e0252b2d7
dodoyoo <dong15903@daum.net>
2025-02-21 17:28:35 +0900

    FEAT: Server 가동 완료

---
+++ server.ts
@@ -0,0 +1,56 @@
+import { Request, Response, NextFunction } from 'express';
+import 'dotenv/config';
+import { AppDataSource } from './src/models/dataSource';
+import { createApp } from './app';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+const app = createApp();
+const port = process.env.PORT || 3000;
+const HOST = process.env.HOST;
+
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Express API with Swagger',
+    version: '1.0.0',
+    description:
+      'A simple CRUD API application made with Express and documented with Swagger',
+  },
+  servers: [
+    {
+      url: `${HOST}:${port}`,
+    },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+  apis: ['./src/domain/user/*.ts'],
+};
+const swaggerSpec = swaggerJSDoc(options);
+
+(async () => {
+  await AppDataSource.initialize()
+    .then(() => {
+      console.log('Data Source has been initialized!');
+
+      app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+      app.all('*', (req: Request, res: Response, next: NextFunction) => {
+        const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+
+        next(err);
+      });
+
+      app.listen(port, async () => {
+        console.log(`Server is running on port ${port}`);
+        console.log(
+          `Swagger docs available at http://localhost:${port}/api-docs`
+        );
+      });
+    })
+    .catch((error) =>
+      console.error('Error during Data Source initialization:', error)
+    );
+})();
