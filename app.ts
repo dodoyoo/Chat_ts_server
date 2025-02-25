110fe4f3b8fdd697a58be1965308fbafc51c65f4
dodoyoo <dong15903@daum.net>
2025-02-25 16:50:56 +0900

    SET: index.js

--- app.ts
+++ app.ts
@@ -1,11 +1,12 @@
 import 'reflect-metadata';
 import 'dotenv/config';
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response, NextFunction, response } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 // import compression from 'compression';
 
 export const createApp = () => {
+  const fs = require('fs');
   const app = express();
 
   app.use(
@@ -16,10 +17,24 @@ export const createApp = () => {
   app.use(morgan('combined'));
   app.use(express.json());
   app.use(express.static('public'));
+  app.use('/css', express.static('./static/css'));
+  app.use('/js', express.static('./static/js'));
   // app.use(compression());
 
   app.get('/ping', (_: Request, res: Response) => {
     res.status(200).json({ message: 'pong' });
+  });
+
+  app.get('/', function (request, response) {
+    fs.readFile('./static/js/index.html', function (err: any, data: any) {
+      if (err) {
+        response.send('에러');
+      } else {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write(data);
+        response.end();
+      }
+    });
   });
 
   return app;
--- server.ts
+++ server.ts
@@ -5,7 +5,8 @@ import { createApp } from './app';
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import { createServer } from 'http';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+import { Any } from 'typeorm';
 
 const app = createApp();
 const port = process.env.PORT || 3000;
@@ -19,18 +20,53 @@ const io = new Server(server, {
   },
 });
 
-io.on('connection', (socket) => {
-  console.log('A user connected:', socket.id);
+interface CustomSocket extends Socket {
+  name?: string; // name 속성 추가
+}
+
+io.on('connection', (socket: CustomSocket) => {
+  socket.on('newUser', (name: string) => {
+    console.log(name + ' 님이 접속하였습니다.');
+
+    socket.name = name; //  name 속성을 안전하게 추가
+
+    io.sockets.emit('update', {
+      type: 'connect',
+      name: 'SERVER',
+      message: `${name} 님이 접속하였습니다.`,
+    });
+  });
 
   socket.on('message', (data) => {
-    console.log('Received message:', data);
-    io.emit('message', data); // 모든 클라이언트에게 메시지 전송
+    if (socket.name) {
+      data.name = socket.name;
+      console.log(data);
+      socket.broadcast.emit('update', data); // 오타 수정 ('updata' → 'update')
+    }
   });
 
   socket.on('disconnect', () => {
-    console.log('A user disconnected:', socket.id);
+    if (socket.name) {
+      console.log(socket.name + ' 님이 나갔습니다.');
+
+      socket.broadcast.emit('update', {
+        type: 'disconnect',
+        name: 'SERVER',
+        message: `${socket.name} 님이 나갔습니다.`,
+      });
+    }
   });
 });
+//   console.log('A user connected:', socket.id);
+
+//   socket.on('message', (data) => {
+//     console.log('Received message:', data);
+//     io.emit('message', data); // 모든 클라이언트에게 메시지 전송
+//   });
+
+//   socket.on('disconnect', () => {
+//     console.log('A user disconnected:', socket.id);
+//   });
 
 const swaggerDefinition = {
   openapi: '3.0.0',
