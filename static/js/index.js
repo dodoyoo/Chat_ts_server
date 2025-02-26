730948126f5f6fdbfb31a931fdb36ad7b6ae3654
dodoyoo <dong15903@daum.net>
2025-02-26 16:06:59 +0900

    FEAT: Chat Service

--- static/js/index.js
+++ static/js/index.js
@@ -1,40 +1,63 @@
 var socket = io();
 
+/* 접속 되었을 때 실행 */
 socket.on('connect', function () {
-  var name = prompt('반갑습니다.', '');
+  /* 이름을 입력받고 */
+  var name = prompt('반갑습니다!', '');
 
+  /* 이름이 빈칸인 경우 */
   if (!name) {
     name = '익명';
   }
 
+  /* 서버에 새로운 유저가 왔다고 알림 */
   socket.emit('newUser', name);
 });
 
+/* 서버로부터 데이터 받은 경우 */
 socket.on('update', function (data) {
-  console.log('받은 데이터:', data); // 데이터 확인용 콘솔 출력
+  var chat = document.getElementById('chat');
 
-  // 채팅 메시지를 화면에 추가하는 함수 호출
-  appendMessage(`${data.name}: ${data.message}`);
+  var message = document.createElement('div');
+  var node = document.createTextNode(`${data.name}: ${data.message}`);
+  var className = '';
+
+  // 타입에 따라 적용할 클래스를 다르게 지정
+  switch (data.type) {
+    case 'message':
+      className = 'other';
+      break;
+
+    case 'connect':
+      className = 'connect';
+      break;
+
+    case 'disconnect':
+      className = 'disconnect';
+      break;
+  }
+
+  message.classList.add(className);
+  message.appendChild(node);
+  chat.appendChild(message);
 });
 
-function appendMessage(message) {
-  var chatBox = document.getElementById('chatBox'); // 채팅 메시지를 표시할 div 찾기
-  if (!chatBox) return; // chatBox가 없으면 아무것도 안 함
-
-  var messageElement = document.createElement('p'); // 새로운 <p> 태그 생성
-  messageElement.textContent = message; // 메시지 내용 추가
-  chatBox.appendChild(messageElement); // chatBox에 추가
-}
-
+/* 메시지 전송 함수 */
 function send() {
+  // 입력되어있는 데이터 가져오기
   var message = document.getElementById('test').value;
 
+  // 가져왔으니 데이터 빈칸으로 변경
   document.getElementById('test').value = '';
 
-  socket.emit('message', { msg: message });
-}
+  // 내가 전송할 메시지 클라이언트에게 표시
+  var chat = document.getElementById('chat');
+  var msg = document.createElement('div');
+  var node = document.createTextNode(message);
+  msg.classList.add('me');
+  msg.appendChild(node);
+  chat.appendChild(msg);
 
-// // // socket.on('connect', function () {
-// // //   var input = document.getElementById('test');
-// // //   input.value = '접속 됨';
-// // // });
+  // 서버로 message 이벤트 전달 + 데이터와 함께
+  socket.emit('message', { type: 'message', message: message });
+}
